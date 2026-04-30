5be8765e4064349ad495c8f88d543e74df86ef1b
CyberDoctor <ilovecyberhk@gmail.com>
2026-05-01 07:52:19 +0800

    Update youtube_timedtext_request.js

--- youtube_timedtext_request.js
+++ youtube_timedtext_request.js
@@ -1,12 +1,5 @@
 let url = $request.url;
 
-const ACTIVE_TTL = 180000; // 3 minutes
-
-function getParam(u, name) {
-  const m = u.match(new RegExp("[?&]" + name + "=([^&]+)"));
-  return m ? decodeURIComponent(m[1]) : "";
-}
-
 function removeParam(u, name) {
   const qIndex = u.indexOf("?");
   if (qIndex === -1) return u;
@@ -25,27 +18,12 @@ function removeParam(u, name) {
   return kept.length ? base + "?" + kept.join("&") : base;
 }
 
-const videoId = getParam(url, "v");
-const target = getParam(url, "tlang");
+// 只负责删除 tlang，避免 YouTube 429
+let newUrl = removeParam(url, "tlang");
+newUrl = removeParam(newUrl, "_yt_x");
+newUrl = removeParam(newUrl, "_yt_trg");
 
-if (target) {
-  const state = JSON.stringify({
-    videoId: videoId || "",
-    target: target,
-    time: Date.now(),
-    ttl: ACTIVE_TTL
-  });
-
-  if (videoId) {
-    $persistentStore.write(state, "yt_translate_state_" + videoId);
-  }
-
-  $persistentStore.write(state, "yt_translate_last_state");
-
-  let newUrl = removeParam(url, "tlang");
-  newUrl = removeParam(newUrl, "_yt_x");
-  newUrl = removeParam(newUrl, "_yt_trg");
-
+if (newUrl !== url) {
   $done({ url: newUrl });
 } else {
   $done({});
