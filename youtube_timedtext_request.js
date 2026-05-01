3085071829f431ae69916b9a50de124e8e25e0cd
CyberDoctor <ilovecyberhk@gmail.com>
2026-05-01 08:41:23 +0800

    Update youtube_timedtext_request.js

--- youtube_timedtext_request.js
+++ youtube_timedtext_request.js
@@ -12,12 +12,21 @@ if (!target) {
 } else {
   const u = new URL(url);
 
+  // 存目标语言，但不放进最终 URL
+  $persistentStore.write(target, "yt_tt_tlang");
+
+  // 删除会触发 429 的参数
   u.searchParams.delete("tlang");
+
+  // 清理旧残留
   u.searchParams.delete("_yt_x");
   u.searchParams.delete("_yt_trg");
+  u.searchParams.delete("dst");
 
+  // 借鉴 DualSubs：只标记当次请求类型
   u.searchParams.set("subtype", "Translate");
-  u.searchParams.set("dst", target);
 
-  $done({ url: u.toString() });
+  $done({
+    url: u.toString()
+  });
 }
