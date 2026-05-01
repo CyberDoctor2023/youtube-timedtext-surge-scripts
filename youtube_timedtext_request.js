7e1febf44cb81d74c6418085fc09ca72a2a257c2
CyberDoctor <ilovecyberhk@gmail.com>
2026-05-01 08:16:00 +0800

    Update youtube_timedtext_request.js

--- youtube_timedtext_request.js
+++ youtube_timedtext_request.js
@@ -1,5 +1,12 @@
 let url = $request.url;
 
+const ACTIVE_TTL = 180000;
+
+function getParam(u, name) {
+  const m = u.match(new RegExp("[?&]" + name + "=([^&]+)"));
+  return m ? decodeURIComponent(m[1]) : "";
+}
+
 function removeParam(u, name) {
   const qIndex = u.indexOf("?");
   if (qIndex === -1) return u;
@@ -18,12 +25,27 @@ function removeParam(u, name) {
   return kept.length ? base + "?" + kept.join("&") : base;
 }
 
-// 只删除会触发 429 的参数
-let newUrl = removeParam(url, "tlang");
-newUrl = removeParam(newUrl, "_yt_x");
-newUrl = removeParam(newUrl, "_yt_trg");
+const videoId = getParam(url, "v");
+const target = getParam(url, "tlang");
 
-if (newUrl !== url) {
+if (target) {
+  const state = JSON.stringify({
+    videoId: videoId || "",
+    target: target,
+    time: Date.now(),
+    ttl: ACTIVE_TTL
+  });
+
+  if (videoId) {
+    $persistentStore.write(state, "yt_translate_state_" + videoId);
+  }
+
+  $persistentStore.write(state, "yt_translate_last_state");
+
+  let newUrl = removeParam(url, "tlang");
+  newUrl = removeParam(newUrl, "_yt_x");
+  newUrl = removeParam(newUrl, "_yt_trg");
+
   $done({ url: newUrl });
 } else {
   $done({});
